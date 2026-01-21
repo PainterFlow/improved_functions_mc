df232f24c3c8476f984bb23f41099351ce087eb9
PainterFlow11 <163044531+PainterFlow@users.noreply.github.com>
2026-01-21 20:54:17 +0100

    a

--- src/main/java/com/example/ipublic_function/mixin/FunctionCommandMixin.java
+++ src/main/java/com/example/ipublic_function/mixin/FunctionCommandMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ServerFunctionManager.class)
-public abstract class FunctionExecutionMixin {
+public abstract class FunctionCommandMixin {
 
     @Inject(
         method = "execute",
